17823a6027dd0dc62e73727f2794988d6e12cfc3
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2017-03-13 18:05:47 +0100

    added pug templates for index page and google analytics loading pug files with pug loader using index.pug template for html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,10 @@ module.exports = {
         }, {
             test: /phaser-arcade-physics\.js$/,
             loader: 'expose-loader?Phaser'
+        }, {
+            test: /\.pug?$/,
+            loader: 'pug-loader',
+            query: { pretty: true }
         }]
     },
     resolve: {
@@ -47,7 +51,25 @@ module.exports = {
             // (with more entries, this ensures that no other module
             //  goes into the vendor chunk)
         }),
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'TODO add title or convert to .ts', //appConfig.title,
+            template: './src/templates/index.pug',
+            data: {
+                description: 'hell yeaaaah description', // TODO read from app config
+                analytics: true, // TODO enable only in production,
+                analyticsId: 'UA-000000-2' // TODO read form app config or remove it from app config
+            },
+            //minify: false
+            /*minify: { // TODO use for production
+                removeComments: true,
+                collapseWhitespace: true,
+                conservativeCollapse: true,
+                minifyJS: {
+                    compress: false,
+                    mangle: false
+                }
+            }*/
+        }),
         new CopyWebpackPlugin([{
             from: 'assets',
             to: 'assets'
